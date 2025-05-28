d87841bd0d8fe57d0953147ca9840ab58481c514
zhipy <zhipeiyuan@bigai.ai>
2025-05-28 16:19:37 +0800

    v5.2

--- src/components/authors.jsx
+++ src/components/authors.jsx
@@ -10,7 +10,7 @@ export default class Authors extends React.Component {
     const columnMaxLen =
       this.props.authors.length > 4 ? 3 : this.props.authors.length;
     const authorClass = `uk-width-1-${columnMaxLen} uk-width-1-${this.props.authors.length}@m`;
-    const affiliationClass = `uk-width-1-${this.props.affiliations.length} uk-margin-small-top`;
+    const affiliationClass = `uk-width-1-1 uk-margin-small-top`;
     return (
       <div>
         <div
